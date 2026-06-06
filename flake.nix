355a04f3cbb36ebbe81b82e254bc47f714acc460
Josh Gustafson <joshgstfsn@gmail.com>
2026-06-06 14:47:39 -0600

    Initial: comfyui-workflow-templates-media-image flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "ComfyUI workflow templates: media-image sub-package";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-lib = {
+      url = "github:jgus/flake-lib/v1";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, flake-lib }:
+    flake-lib.lib.mkLeafFlake {
+      inherit nixpkgs flake-utils;
+      source = { type = "pypi"; pname = "comfyui_workflow_templates_media_image"; format = "sdist"; };
+      package = {
+        attr = "comfyui-workflow-templates-media-image";
+        description = "ComfyUI workflow templates: media-image sub-package";
+        extra = { dontCheckRuntimeDeps = true; };
+      };
+      pin = import ./pin.nix;
+      branches = false;
+    };
+}
